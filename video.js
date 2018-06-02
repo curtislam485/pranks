808d82238b723ad3b647100be4b3f8f1f9f3c804
curtislam485 <39864295+curtislam485@users.noreply.github.com>
2018-06-02 12:44:45 -0700

    Create video.js
    replace website with a video

---
+++ video.js
@@ -0,0 +1,2 @@
+var body =  document.querySelector("body");
+body.innerHTML = '<video width="100%" controls autoplay><source src="http://dl3.webmfiles.org/big-buck-bunny_trailer.webm" type="video/webm"></video>';
